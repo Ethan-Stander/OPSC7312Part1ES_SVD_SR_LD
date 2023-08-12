b4d8ea69207efd555a7e65514eb15276066acac7
OofSpooder <ethanstander55@gmail.com>
2023-08-12 10:08:48 +0200

    Edited API methodds

--- app/src/main/java/com/example/opsc7312part1/APIServices.kt
+++ app/src/main/java/com/example/opsc7312part1/APIServices.kt
@@ -1,57 +1,76 @@
 package com.example.opsc7312part1
 
+import android.provider.ContactsContract.CommonDataKinds.Website.URL
+import com.google.gson.Gson
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import kotlinx.coroutines.runBlocking
+import java.net.URL
 
 
 class APIServices {
 
     companion object {
-        private val httpClient = HttpClient()
+        private val httpClient = (CIO)
         private const val JSON_URL_DATA = "192.168.1.10/sensor.json"
         private const val JSON_URL_HARDWARE = "192.168.1.10/hardware.json"
 
         suspend fun fetchSensorDataFromJson(): SensorDataAPI? {
-            return withContext(Dispatchers.IO) {
-                val response: HttpResponse = httpClient.get(JSON_URL_DATA)
-                if (response.status.value == 200) {
-                    val json = response.bodyAsText()
-                    val sensorData = Json.decodeFromString<SensorDataAPI>(json)
-                    return@withContext sensorData
-                } else {
-                    // Handle non-200 response status if needed
+            return withContext(Dispatchers.IO)
+            {
+
+                var data: SensorDataAPI? = null
+                try {
+                    val url = URL(JSON_URL_DATA);
+                    val json = url.readText()
+                     data = Gson().fromJson(json, SensorDataAPI::class.java)
+                }
+                catch(e:Exception)
+                {
                     return@withContext null
                 }
+
+            if(data != null) {
+                return@withContext data
             }
+            else
+                return@withContext null
+            }
+
         }
 
-        suspend fun fetchHardware(): hardware? {
-            return withContext(Dispatchers.IO) {
-                val response: HttpResponse = httpClient.get(JSON_URL_HARDWARE)
-                if (response.status.value == 200) {
-                    val json = response.bodyAsText()
-                    val hardware = Json.decodeFromString<hardware>(json)
-                    return@withContext hardware
-                } else {
-                    // Handle non-200 response status if needed
+        suspend fun fetchhardware() : hardware?
+        {
+            return withContext(Dispatchers.IO)
+            {
+
+                var Hardware : hardware? = null
+                try {
+                    val url = URL(JSON_URL_HARDWARE)
+                    val json = url.readText()
+                    Hardware = Gson().fromJson(json, hardware::class.java)
+                }
+                catch (e:Exception)
+                {
                     return@withContext null
                 }
+                if(Hardware != null)
+                {
+                    return@withContext Hardware
+                }
+                else
+                    return@withContext null
             }
         }
 
-        suspend fun GenericSwitch(URL : String): Boolean {
-            return withContext(Dispatchers.IO){
-                val response: HttpResponse = httpClient.get(URL)
-                return@withContext response.status.value == 200
-            }
 
-
-        }
 
 
 
