f4ef05c246b61a124e50b7b52db6478a711ba9e3
STEFAN10085288 <svdbegr0108@gmail.com>
2023-08-22 18:38:04 +0200

    updated status readings of sensor data

--- app/src/main/java/com/example/opsc7312part1/EquipmentStatusData.kt
+++ app/src/main/java/com/example/opsc7312part1/EquipmentStatusData.kt
@@ -1,3 +1,3 @@
 package com.example.opsc7312part1
 
-data class EquipmentStatusData(var equipmentTitle:String, var equipmentStatus: Boolean)
+data class EquipmentStatusData(var equipmentTitle:String, var equipmentStatus: Boolean, var link : String)
