f5f5c9278b628b4c93cd76ccc8215d30ffaea0c2
Stephan Vorster <stephanrvorster@gmail.com>
2023-08-26 19:05:48 +0200

    Added Google Login Page and icons

---
+++ app/src/main/java/com/example/opsc7312part1/GoogleLogin.kt
@@ -0,0 +1,13 @@
+package com.example.opsc7312part1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_login)
+
+
+    }
+}
