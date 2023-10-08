76cfa7a59003972c92efd8c7ac42882ce48f4914
STEFAN10085288 <svdbegr0108@gmail.com>
2023-10-08 12:42:20 +0200

    added clearData method to delete button on account fragment

--- app/src/main/java/com/example/opsc7312part1/AccountPopupFragment.kt
+++ app/src/main/java/com/example/opsc7312part1/AccountPopupFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
 import com.example.opsc7312part1.FirebaseUtils.Companion.deleteUser
@@ -61,8 +62,7 @@ class AccountPopupFragment : DialogFragment() {
                         startActivity(googleSignInIntent)
 
                     } else {
-                        // Failed to delete the user, you can handle the error here
-                        // For example:
+                        Toast.makeText(requireActivity(),"Failed to delete user",Toast.LENGTH_SHORT).show()
 
                     }
                 }
