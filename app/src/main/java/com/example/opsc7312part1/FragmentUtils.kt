e149f3b71bf6e3f27a44960f8439e71dfe41b350
Stephan-Robert <stephanrvorster@gmail.com>
2023-10-16 15:37:57 +0200

    Sensor and equipment pages refresh

--- app/src/main/java/com/example/opsc7312part1/FragmentUtils.kt
+++ app/src/main/java/com/example/opsc7312part1/FragmentUtils.kt
@@ -1,20 +1,27 @@
 package com.example.opsc7312part1
 
 import android.content.Context
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class FragmentUtils {
+    companion object {
+        fun refreshFragment(activity: FragmentActivity, fragment: Fragment, containerId: Int) {
+            val fragmentManager: FragmentManager = activity.supportFragmentManager
+            val transaction: FragmentTransaction = fragmentManager.beginTransaction()
 
-    fun refreshFragment(context:Context?, currentFragment: Fragment){
-        context?.let {
-            val fragmentManager = (context as? AppCompatActivity)?.supportFragmentManager
-            fragmentManager?.let {
-                    val fragmentTransaction = fragmentManager.beginTransaction()
-                    fragmentTransaction.detach(currentFragment)
-                    fragmentTransaction.attach(currentFragment)
-                    fragmentTransaction.commit()
-            }
+            // Create a new instance of the fragment
+            val newFragment = fragment.javaClass.newInstance()
+
+            // Replace the existing fragment with the new one
+            transaction.replace(containerId, newFragment)
+
+            // Commit the transaction
+            transaction.commit()
         }
     }
 }
