1f8c2210665688e1b6535cf981c87b608014f83d
STEFAN10085288 <svdbegr0108@gmail.com>
2023-11-08 13:01:35 +0200

    Journal creation works completely

--- app/src/main/java/com/example/opsc7312part1/JournalEntryDetailsFragment.kt
+++ app/src/main/java/com/example/opsc7312part1/JournalEntryDetailsFragment.kt
@@ -1,18 +1,24 @@
 package com.example.opsc7312part1
 
+import android.app.Activity.RESULT_CANCELED
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -20,10 +26,17 @@ import java.util.Locale
 
 class JournalEntryDetailsFragment : DialogFragment() {
 
-
+    private lateinit var btnExit : ImageView
     private lateinit var btnAddPhoto : Button
     private lateinit var journalPhoto : ImageView
     private lateinit var btnSelectDate : Button
+    private  lateinit var  btnSaveEntry : Button
+    private lateinit var txtTitle : EditText
+    private lateinit var  txtNotes : EditText
+    private var entryDate : String = ""
+    private var photoBitMap : Bitmap? = null
+    private var photoUri : Uri? = null
+    private var imageLink :String= ""
 
     //for datepicker
     private val calendar = Calendar.getInstance()
@@ -49,9 +62,17 @@ class JournalEntryDetailsFragment : DialogFragment() {
             dismiss()
         }
 
+        btnExit = view.findViewById(R.id.journalCreateExitButton)
         btnAddPhoto = view.findViewById(R.id.btnTakePhoto)
         journalPhoto = view.findViewById(R.id.journalTakenPhoto)
         btnSelectDate = view.findViewById(R.id.btnSelectDate)
+        btnSaveEntry = view .findViewById(R.id.btnSaveEntry)
+        txtTitle = view.findViewById(R.id.edt_journal_create_title)
+        txtNotes = view.findViewById(R.id.edt_journal_create_notes)
+
+        btnExit.setOnClickListener {
+            dismiss()
+        }
 
         btnAddPhoto.setOnClickListener {
             takePhoto()
@@ -60,7 +81,53 @@ class JournalEntryDetailsFragment : DialogFragment() {
         btnSelectDate.setOnClickListener {
             selectDate()
         }
+
+        btnSaveEntry.setOnClickListener {
+            JournalUtils.saveJournalImage(photoUri) { photoLink ->
+                imageLink = photoLink
+                saveEntry()
+            }
+
+        }
     }
+
+    private fun saveEntry() {
+        //user object
+        val user = User(
+            UserID = UserID,
+            Username = UserName
+        )
+
+        //journal object
+        val journal = Journal(
+            title = txtTitle.text.toString(),
+            notes = txtNotes.text.toString(),
+            date = entryDate,
+            imageLink = imageLink
+        )
+
+        lifecycleScope.launch {
+            try {
+                if (journal.title.isNullOrEmpty() || journal.notes.isNullOrEmpty() || journal.date.isNullOrEmpty() || journal.imageLink.isNullOrEmpty()) {
+                    Toast.makeText(requireContext(), "Please ensure all fields are filled in .", Toast.LENGTH_SHORT).show()
+                }else
+                {
+                    val result = JournalUtils.saveJournalEntry(user, journal)
+                    if (result) {
+                        Toast.makeText(requireContext(), "Entry saved successfully.", Toast.LENGTH_SHORT).show()
+                        dismiss()
+                    } else {
+                        Toast.makeText(requireContext(), "Failed to save entry.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } catch (e: Exception) {
+                Toast.makeText(requireContext(), "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+
 
     //lets user pick the date of the journal entry
     private fun selectDate() {
@@ -70,7 +137,8 @@ class JournalEntryDetailsFragment : DialogFragment() {
                 selectedDate.set(year, monthOfYear, dayOfMonth)
                 val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
                 val formattedDate = dateFormat.format(selectedDate.time)
-                btnSelectDate.text = "Selected Date: $formattedDate"
+                entryDate =  formattedDate.toString()
+                btnSelectDate.text = entryDate
             },
             calendar.get(Calendar.YEAR),
             calendar.get(Calendar.MONTH),
@@ -89,10 +157,29 @@ class JournalEntryDetailsFragment : DialogFragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == pic_id) {
-            val photo = data!!.extras!!["data"] as Bitmap?
-            journalPhoto.setImageBitmap(photo)
+        if(resultCode != RESULT_CANCELED) {
+            if (requestCode == pic_id) {
+
+                photoBitMap = data!!.extras!!["data"] as Bitmap?
+
+                //converts bitmap to uri
+                val bytes = ByteArrayOutputStream()
+                photoBitMap?.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+                val path: String = MediaStore.Images.Media.insertImage(
+                    requireContext().getContentResolver(),
+                    photoBitMap,
+                    "Title",
+                    null
+                )
+                photoUri =  Uri.parse(path)
+
+                if (photoUri != null) {
+                    journalPhoto.setImageURI(photoUri)
+                    Log.i("Check phot uri:", photoUri.toString())
+                }
+            }
         }
+
     }
     // Define the variable pic_id which is the request-id of the clicked image.
     companion object {
--- app/src/main/java/com/example/opsc7312part1/JournalUtils.kt
+++ app/src/main/java/com/example/opsc7312part1/JournalUtils.kt
@@ -1,26 +1,53 @@
 package com.example.opsc7312part1
 
+import android.net.Uri
+import android.util.Log
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.net.URI
 
 class JournalUtils {
 
     companion object {
 
         val database = FirebaseDatabase.getInstance()
+        var storage = FirebaseStorage.getInstance().reference.child("Images")
         suspend fun saveJournalEntry(user: User, journalEntry: Journal): Boolean {
             val currentUser = user.UserID
             val reference =
-                currentUser?.let { FirebaseUtils.database.getReference("Users").child(it).child("journalEntries") }
+                currentUser?.let { FirebaseUtils.database.getReference("Users").child(it).child("journalEntries")}
 
             return try {
                 if (reference != null) {
-                    reference.setValue(journalEntry).await()
+                    val entryRef = reference.push()
+                    entryRef.setValue(journalEntry).await()
                 }
                 true
             } catch (e: Exception) {
                 false
             }
         }
+
+        fun saveJournalImage(imageURI : Uri?, callback: (String) -> Unit) {
+            imageURI?.let {
+                val fileReference = storage.child(System.currentTimeMillis().toString())
+                fileReference.putFile(it).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        fileReference.downloadUrl.addOnSuccessListener { uri ->
+                            val photoLink = uri.toString()
+                            callback(photoLink)
+                        }
+                    } else {
+                        Log.i("image upload failed: ", task.exception?.message.toString())
+                        callback("")
+                    }
+                }
+            } ?: callback("")
+        }
+
+
     }
 }
